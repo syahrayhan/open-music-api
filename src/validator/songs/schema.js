2778685a8a98cab0d6c4aa59236d708145b643b5
Rayhan <syahrayhanid@gmail.com>
2021-07-30 09:12:18 +0700

    add specific validator

--- src/validator/songs/schema.js
+++ src/validator/songs/schema.js
@@ -2,7 +2,7 @@ const Joi = require('joi')
 
 const OpenMusicPayloadSchema = Joi.object({
   title: Joi.string().required(),
-  year: Joi.number().required(),
+  year: Joi.number().integer().min(1500).max(4444).required(),
   performer: Joi.string().required(),
   genre: Joi.string().required(),
   duration: Joi.string().required(),
