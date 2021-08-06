5f6be764196ff3d02b9fe1de70e7856164154a94
root <root@DESKTOP-45HCV7D.localdomain>
2021-08-06 12:43:06 +0700

    add users plugin

--- src/api/users/handler.js
+++ src/api/users/handler.js
@@ -2,6 +2,8 @@ class UsersHandler {
   constructor (service, validator) {
     this._service = service
     this._validator = validator
+
+    this.postUserHandler = this.postUserHandler.bind(this)
   }
 
   async postUserHandler (request, h) {
