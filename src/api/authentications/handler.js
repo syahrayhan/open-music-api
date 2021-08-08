65cb9a9999475f35482e4dd73b8c57bf3ef92e91
Rayhan <syahrayhanid@gmail.com>
2021-08-09 06:35:31 +0700

    add authentication binding

--- src/api/authentications/handler.js
+++ src/api/authentications/handler.js
@@ -4,6 +4,10 @@ class AuthenticationsHandler {
     this._usersService = usersService
     this._tokenManager = tokenManager
     this._validator = validator
+
+    this.postAuthenticationHandler = this.postAuthenticationHandler.bind(this)
+    this.putAuthenticationHandler = this.putAuthenticationHandler.bind(this)
+    this.deleteAuthenticationHandler = this.deleteAuthenticationHandler.bind(this)
   }
 
   async postAuthenticationHandler (request, h) {
