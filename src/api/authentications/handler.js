a9a90327c5bf5a4d41e4da02680227901de19395
Rayhan <syahrayhanid@gmail.com>
2021-08-09 06:14:08 +0700

    add authentication service, with post put and delete api function

--- src/api/authentications/handler.js
+++ src/api/authentications/handler.js
@@ -10,5 +10,52 @@ class AuthenticationsHandler {
     this._validator.validatePostAuthenticationPayload(request.payload)
 
     const { username, password } = request.payload
+    const id = await this._usersService.verifyUserCredential(username, password)
+    const accessToken = this._tokenManager.generateAccessToken({ id })
+    const refreshToken = this._tokenManager.generateRrefreshToken({ id })
+
+    await this._authenticationsService.addRefreshToken(refreshToken)
+
+    return h.response({
+      status: 'success',
+      message: 'Authentication berhasil ditambahkan',
+      data: {
+        accessToken,
+        refreshToken,
+      },
+    }).code(201)
+  }
+
+  async putAuthenticationHandler (request) {
+    this._validator.validatePutAuthenticationPayload(request.payload)
+
+    const { refreshToken } = request.payload
+    await this._authenticationsService.verifyRefreshToken(refreshToken)
+    const { id } = this._tokenManager.verifyRefreshToken(refreshToken)
+
+    const accessToken = this._tokenManager.generateAccessToken({ id })
+
+    return {
+      status: 'success',
+      message: 'Authentication berhasil diperbarui',
+      data: {
+        accessToken,
+      },
+    }
+  }
+
+  async deleteAuthenticationHandler (request) {
+    this._validator.validateDeleteAuthenticationPayload(request.payload)
+
+    const { refreshToken } = request.payload
+    await this._authenticationsService.verifyRefreshToken(refreshToken)
+    await this._authenticationsService.deleteRefreshToken(refreshToken)
+
+    return {
+      status: 'success',
+      message: 'Refresh token berhasil dihapus',
+    }
   }
 }
+
+module.exports = AuthenticationsHandler
