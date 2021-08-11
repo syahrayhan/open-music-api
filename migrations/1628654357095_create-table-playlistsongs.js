2e3c6c462c228f7fa27568ee957c58263bb06179
root <root@DESKTOP-45HCV7D.localdomain>
2021-08-11 11:11:44 +0700

    create table playlistsongs

--- migrations/1628654357095_create-table-playlistsongs.js
+++ migrations/1628654357095_create-table-playlistsongs.js
@@ -1,5 +1,36 @@
 /* eslint-disable camelcase */
 
-exports.up = pgm => {}
+exports.up = pgm => {
+  pgm.createTable('playlistsongs', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    playlist_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    song_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+  })
 
-exports.down = pgm => {}
+  pgm.addConstraint(
+    'playlistsongs',
+    'fk_playlistsongs.playlist_id_playlists.id',
+    'FOREIGN KEY(playlist_id) REFERENCES playlists(id) ON DELETE CASCADE'
+  )
+
+  pgm.addConstraint(
+    'playlistsongs',
+    'fk_playlistsongs.song_id_music.id',
+    'FOREIGN KEY(song_id) REFERENCES music(id) ON DELETE CASCADE'
+  )
+}
+
+exports.down = pgm => {
+  pgm.dropTable('playlistsongs')
+  pgm.dropConstraint('playlistsongs', 'fk_playlistsongs.playlist_id_playlists.id')
+  pgm.dropConstraint('playlistsongs', 'fk_playlistsongs.song_id_music.id')
+}
