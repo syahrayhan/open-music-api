f62717081316a20ec56db37ecddecd2b5094526a
Rayhan <syahrayhanid@gmail.com>
2021-08-21 07:23:00 +0700

    add exports validator

---
+++ src/api/exports/handler.js
@@ -0,0 +1,11 @@
+class ExportsHandler {
+  constructor (service, validator) {
+    this._service = service
+    this._validator = validator
+  }
+
+  async postExportMusicHandler (request, h) {
+  }
+}
+
+module.exports = ExportsHandler
